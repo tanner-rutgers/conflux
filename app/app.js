45905e1e66612e62687310a633ef356eb4c6b072
Tanner Rutgers <tanner_rutgers@intuit.com>
2015-11-21 12:02:25 -0800

    Added logging framework (morgan + winston)

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var morgan = require('morgan');
+var logger = require("./utils/logger");
 
 var app = express();
+
+// Configuration
+app.use(morgan('common', {stream: logger.stream}));
 
 module.exports = app;
