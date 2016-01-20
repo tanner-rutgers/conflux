383d5a821ba4cee7a8b9e1bc6c780f7163474dd5
Tanner Rutgers <tanner_rutgers@intuit.com>
2016-01-19 18:54:30 -0800

    Added movies endpoint with search capability

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var logger = require("./utils/logger");
-
+var bodyParser = require('body-parser')
 var app = express();
 
 // Configure logger
@@ -11,9 +11,26 @@ morgan.token('body', function(req) {
 });
 app.use(morgan('[:date[web]] :method url: :url, content-type: :res[content-type], body: :body, status: :status, response-time: :response-time ms', {stream: logger.stream}));
 
+// Basic configuration
+app.use(bodyParser.json());
+
 // Routes setup
 logger.info("Configuring routes");
 var apiRouter = require('./routes/api');
 app.use('/api', apiRouter);
+
+// Error handling
+app.use(logErrors);
+app.use(errorHandler);
+
+function logErrors(err, req, res, next) {
+    logger.error("Application error", err.stack);
+    next(err);
+}
+
+function errorHandler(err, req, res, next) {
+    res.status(500);
+    res.json({ error: err });
+}
 
 module.exports = app;
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var logger = require("../utils/logger");
+var MoviesController = require('../controllers/moviesController');
 
-router.get('/', function (req, res) {
-    res.send("Nothing here!");
+router.get('/movies', function (request, response) {
+    var query = request.query.query;
+    var from = request.query.from;
+    var size = request.query.size;
+    var filters = getFilters(request);
+    var moviesController = new MoviesController();
+    moviesController.searchAll(query, filters, from, size, function(err, res) {
+        if (err) {
+            logger.error("Error calling movies.searchAll", err);
+            response.status(500).end();
+        }
+
+        response.status(200).send(res);
+    })
 });
+
+function getFilters(request) {
+    var filters = [];
+    for (var propName in request.query) {
+        if (request.query.hasOwnProperty(propName) &&
+            propName != 'query' &&
+            propName != 'from' &&
+            propName != 'size') {
+            filters.push({propName: request.query.propName});
+        }
+    }
+    return filters;
+}
 
 module.exports = router;
